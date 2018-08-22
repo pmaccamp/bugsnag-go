d5968b9ea24c337e60cb0f4747bc38ef0b571007
Ben Ibinson <codehex.net@gmail.com>
2018-08-22 20:46:35 +0100

    [feat] Enable sessions for frameworks

--- bugsnag.go
+++ bugsnag.go
@@ -20,7 +20,6 @@ import (
 
 // The current version of bugsnag-go.
 const VERSION = "1.3.1"
-const configuredMultipleTimes = "WARNING: Bugsnag was configured twice. It is recommended to only call bugsnag.Configure once to ensure consistent session tracking behavior"
 
 var once sync.Once
 var middleware middlewareStack
@@ -43,12 +42,12 @@ var sessionTracker sessions.SessionTracker
 func Configure(config Configuration) {
 	Config.update(&config)
 	once.Do(Config.PanicHandler)
-	startSessionTracking()
 }
 
 // StartSession creates a clone of the context.Context instance with Bugsnag
-// session data attached.
+// session data attached. Will start the session tracker if not already started
 func StartSession(ctx context.Context) context.Context {
+	startSessionTracking()
 	return sessionTracker.StartSession(ctx)
 }
 
@@ -113,8 +112,15 @@ func Handler(h http.Handler, rawData ...interface{}) http.Handler {
 	}
 
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		defer notifier.AutoNotify(r)
-		h.ServeHTTP(w, r)
+		request := r
+
+		// Record a session if auto notify session is enabled
+		if Config.IsAutoCaptureSessions() {
+			ctx := StartSession(r.Context())
+			request = r.WithContext(ctx)
+		}
+		defer notifier.AutoNotify(request)
+		h.ServeHTTP(w, request)
 	})
 }
 
@@ -128,8 +134,14 @@ func HandlerFunc(h http.HandlerFunc, rawData ...interface{}) http.HandlerFunc {
 	notifier := New(rawData...)
 
 	return func(w http.ResponseWriter, r *http.Request) {
-		defer notifier.AutoNotify(r)
-		h(w, r)
+		request := r
+		// Record a session if auto notify session is enabled
+		if Config.IsAutoCaptureSessions() {
+			ctx := StartSession(r.Context())
+			request = r.WithContext(ctx)
+		}
+		defer notifier.AutoNotify(request)
+		h(w, request)
 	}
 }
 
@@ -172,21 +184,19 @@ func init() {
 }
 
 func startSessionTracking() {
-	sessionTrackingConfig.Update(&sessions.SessionTrackingConfiguration{
-		APIKey:          Config.APIKey,
-		Endpoint:        Config.Endpoints.Sessions,
-		Version:         VERSION,
-		PublishInterval: DefaultSessionPublishInterval,
-		Transport:       Config.Transport,
-		ReleaseStage:    Config.ReleaseStage,
-		Hostname:        Config.Hostname,
-		AppType:         Config.AppType,
-		AppVersion:      Config.AppVersion,
-		Logger:          Config.Logger,
-	})
-	if sessionTracker != nil {
-		Config.logf(configuredMultipleTimes)
-	} else {
+	if sessionTracker == nil {
+		sessionTrackingConfig.Update(&sessions.SessionTrackingConfiguration{
+			APIKey:          Config.APIKey,
+			Endpoint:        Config.Endpoints.Sessions,
+			Version:         VERSION,
+			PublishInterval: DefaultSessionPublishInterval,
+			Transport:       Config.Transport,
+			ReleaseStage:    Config.ReleaseStage,
+			Hostname:        Config.Hostname,
+			AppType:         Config.AppType,
+			AppVersion:      Config.AppVersion,
+			Logger:          Config.Logger,
+		})
 		sessionTracker = sessions.NewSessionTracker(&sessionTrackingConfig)
 	}
 }
--- bugsnag_test.go
+++ bugsnag_test.go
@@ -16,14 +16,14 @@ import (
 
 func TestConfigure(t *testing.T) {
 	Configure(Configuration{
-		APIKey: testAPIKey,
+		APIKey: "random-key",
 	})
 
-	if Config.APIKey != testAPIKey {
+	if Config.APIKey != "random-key" {
 		t.Errorf("Setting APIKey didn't work")
 	}
 
-	if New().Config.APIKey != testAPIKey {
+	if New().Config.APIKey != "random-key" {
 		t.Errorf("Setting APIKey didn't work for new notifiers")
 	}
 }
@@ -49,6 +49,9 @@ func startTestServer() {
 			panic(err)
 		}
 		testEndpoint = "http://" + l.Addr().String() + "/"
+		Configure(Configuration{
+			Endpoints: Endpoints{Notify: testEndpoint, Sessions: testEndpoint},
+		})
 
 		go http.Serve(l, mux)
 	})
@@ -74,7 +77,6 @@ func TestNotify(t *testing.T) {
 	Notify(fmt.Errorf("hello world"),
 		Configuration{
 			APIKey:          testAPIKey,
-			Endpoints:       Endpoints{Notify: testEndpoint},
 			ReleaseStage:    "test",
 			AppType:         "foo",
 			AppVersion:      "1.2.3",
@@ -184,10 +186,11 @@ func runCrashyServer(rawData ...interface{}) (net.Listener, error) {
 
 func TestHandler(t *testing.T) {
 	startTestServer()
+	DefaultSessionPublishInterval = time.Millisecond * 10
+	Configure(Configuration{AutoCaptureSessions: false})
 
 	l, err := runCrashyServer(Configuration{
 		APIKey:          testAPIKey,
-		Endpoints:       Endpoints{Notify: testEndpoint},
 		ProjectPackages: []string{"github.com/bugsnag/bugsnag-go"},
 		Logger:          log.New(ioutil.Discard, log.Prefix(), log.Flags()),
 	}, SeverityInfo)
@@ -262,9 +265,121 @@ func TestHandler(t *testing.T) {
 		frame3.Get("lineNumber").MustInt() == 0 {
 		t.Errorf("Wrong frame3: %v", frame3)
 	}
+
+	// Check that a session was not sent
+	select {
+	case _ = <-postedJSON:
+		t.Errorf("Session was sent when auto capture sessions is switched off")
+	case <-time.After(200 * time.Millisecond):
+		return
+	}
+}
+
+func TestHandlerWithSessions(t *testing.T) {
+	startTestServer()
+	DefaultSessionPublishInterval = time.Millisecond * 10
+	Configure(Configuration{AutoCaptureSessions: true})
+
+	l, err := runCrashyServer(Configuration{
+		APIKey:          testAPIKey,
+		ProjectPackages: []string{"github.com/bugsnag/bugsnag-go"},
+		Logger:          log.New(ioutil.Discard, log.Prefix(), log.Flags()),
+	}, SeverityInfo)
+	if err != nil {
+		t.Fatal(err)
+	}
+	http.Get("http://" + l.Addr().String() + "/ok?foo=bar")
+	l.Close()
+
+	json, err := simplejson.NewJson(<-postedJSON)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	jsonSession, err := simplejson.NewJson(<-postedJSON)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if json.Get("apiKey").MustString() != testAPIKey {
+		t.Errorf("Wrong api key in payload")
+	}
+
+	if json.GetPath("notifier", "name").MustString() != "Bugsnag Go" {
+		t.Errorf("Wrong notifier name in payload")
+	}
+
+	event := json.Get("events").GetIndex(0)
+
+	// TODO : Add test to check session ID in event
+	for k, value := range map[string]string{
+		"payloadVersion":              "2",
+		"severity":                    "info",
+		"user.id":                     "127.0.0.1",
+		"metaData.request.url":        "http://" + l.Addr().String() + "/ok?foo=bar",
+		"metaData.request.httpMethod": "GET",
+	} {
+		key := strings.Split(k, ".")
+		if event.GetPath(key...).MustString() != value {
+			t.Errorf("Wrong %v: %v != %v", key, event.GetPath(key...).MustString(), value)
+		}
+	}
+
+	for k, value := range map[string]string{
+		"notifier.name":    "Bugsnag Go",
+		"notifier.url":     "https://github.com/bugsnag/bugsnag-go",
+		"app.releaseStage": "production",
+	} {
+		key := strings.Split(k, ".")
+		if jsonSession.GetPath(key...).MustString() != value {
+			t.Errorf("Wrong %v: %v != %v", key, event.GetPath(key...).MustString(), value)
+		}
+	}
+
+	if jsonSession.GetPath("sessionCounts", "sessionsStarted").MustInt() != 1 {
+		t.Errorf("Wrong %v: %v != %v", "sessionCounts.sessionsStarted", jsonSession.GetPath("sessionCounts", "sessionsStarted").MustInt(), 1)
+	}
+
+	if event.GetPath("metaData", "request", "params", "foo").GetIndex(0).MustString() != "bar" {
+		t.Errorf("missing GET params in request metadata")
+	}
+
+	if event.GetPath("metaData", "request", "headers", "Accept-Encoding").GetIndex(0).MustString() != "gzip" {
+		t.Errorf("missing GET params in request metadata: %v", event.GetPath("metaData", "request", "headers"))
+	}
+
+	exception := event.Get("exceptions").GetIndex(0)
+	if !strings.Contains(exception.Get("message").MustString(), "send on closed channel") {
+		t.Errorf("Wrong message in payload: %v '%v'", exception.Get("message").MustString(), "runtime error: send on closed channel")
+	}
+
+	errorClass := exception.Get("errorClass").MustString()
+	if errorClass != "runtime.errorCString" && errorClass != "*errors.errorString" && errorClass != "runtime.plainError" {
+		t.Errorf("Wrong errorClass in payload: %v, expected '%v', '%v', '%v'",
+			exception.Get("errorClass").MustString(),
+			"runtime.errorCString", "*errors.errorString", "runtime.plainError")
+	}
+
+	frame0 := exception.Get("stacktrace").GetIndex(0)
+
+	file0 := frame0.Get("file").MustString()
+	if !strings.HasPrefix(file0, "runtime/panic") ||
+		frame0.Get("inProject").MustBool() != false {
+		t.Errorf("Wrong frame0: %v", frame0)
+	}
+
+	frame3 := exception.Get("stacktrace").GetIndex(3)
+
+	if frame3.Get("file").MustString() != "bugsnag_test.go" ||
+		frame3.Get("method").MustString() != "crashyHandler" ||
+		frame3.Get("inProject").MustBool() != true ||
+		frame3.Get("lineNumber").MustInt() == 0 {
+		t.Errorf("Wrong frame3: %v", frame3)
+	}
 }
 
 func TestAutoNotify(t *testing.T) {
+	startTestServer()
 
 	var panicked interface{}
 
@@ -272,7 +387,7 @@ func TestAutoNotify(t *testing.T) {
 		defer func() {
 			panicked = recover()
 		}()
-		defer AutoNotify(Configuration{Endpoints: Endpoints{Notify: testEndpoint}, APIKey: testAPIKey})
+		defer AutoNotify(Configuration{APIKey: testAPIKey})
 
 		panic("eggs")
 	}()
@@ -300,13 +415,14 @@ func TestAutoNotify(t *testing.T) {
 }
 
 func TestRecover(t *testing.T) {
+	startTestServer()
 	var panicked interface{}
 
 	func() {
 		defer func() {
 			panicked = recover()
 		}()
-		defer Recover(Configuration{Endpoints: Endpoints{Notify: testEndpoint}, APIKey: testAPIKey})
+		defer Recover(Configuration{APIKey: testAPIKey})
 
 		panic("ham")
 	}()
@@ -367,7 +483,7 @@ func ExampleRecover() {
 	}
 
 	func() {
-		defer Recover(Configuration{Endpoints: Endpoints{Notify: testEndpoint}, APIKey: testAPIKey})
+		defer Recover(Configuration{APIKey: testAPIKey})
 		job.Process()
 	}()
 	fmt.Println("Panic recovered")
@@ -510,20 +626,6 @@ type logger struct{ msg string }
 
 func (l *logger) Printf(format string, v ...interface{}) { l.msg = format }
 
-func TestConfigureTwice(t *testing.T) {
-	sessionTracker = nil
-
-	l := logger{}
-	Configure(Configuration{Logger: &l})
-	if l.msg != "" {
-		t.Errorf("unexpected log message: %s", l.msg)
-	}
-	Configure(Configuration{})
-	if got, exp := l.msg, configuredMultipleTimes; exp != got {
-		t.Errorf("unexpected log message: '%s', expected '%s'", got, exp)
-	}
-}
-
 func assertSeverityReasonEqual(t *testing.T, json *simplejson.Json, expSeverity string, reasonType string, expUnhandled bool) {
 	event := json.Get("events").GetIndex(0)
 	reason := event.GetPath("severityReason", "type").MustString()
@@ -546,7 +648,7 @@ func assertSeverityReasonEqual(t *testing.T, json *simplejson.Json, expSeverity 
 func generateSampleConfig() Configuration {
 	return Configuration{
 		APIKey:          testAPIKey,
-		Endpoints:       Endpoints{Notify: testEndpoint},
+		Endpoints:       Endpoints{Notify: testEndpoint, Sessions: testEndpoint},
 		ReleaseStage:    "test",
 		AppType:         "foo",
 		AppVersion:      "1.2.3",
