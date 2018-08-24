a8a142c9ef641d2376cd088282dcea5bdfc57348
Ben Ibinson <codehex.net@gmail.com>
2018-08-24 17:00:32 +0100

    [fix] Record handled state correctly

--- notifier.go
+++ notifier.go
@@ -82,7 +82,7 @@ func (notifier *Notifier) AutoNotify(rawData ...interface{}) {
 	if err := recover(); err != nil {
 		severity := notifier.getDefaultSeverity(rawData, SeverityError)
 		state := HandledState{SeverityReasonHandledPanic, severity, true, ""}
-		notifier.appendStateIfNeeded(rawData, state)
+		rawData = notifier.appendStateIfNeeded(rawData, state)
 		notifier.Notify(append(rawData, errors.New(err, 2))...)
 		panic(err)
 	}
@@ -95,7 +95,7 @@ func (notifier *Notifier) Recover(rawData ...interface{}) {
 	if err := recover(); err != nil {
 		severity := notifier.getDefaultSeverity(rawData, SeverityWarning)
 		state := HandledState{SeverityReasonHandledPanic, severity, false, ""}
-		notifier.appendStateIfNeeded(rawData, state)
+		rawData = notifier.appendStateIfNeeded(rawData, state)
 		notifier.Notify(append(rawData, errors.New(err, 2))...)
 	}
 }
